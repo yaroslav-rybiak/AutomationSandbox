3124d4c5d48dfebc896403aed2627ba187e4283a
Yaroslav Rybiak <yaroslav.rybiak@gmail.com>
2015-08-26 21:19:11 +0300

    credentials

--- src/Data/Credentials.java
+++ src/Data/Credentials.java
@@ -2,8 +2,8 @@ package Data;
 
 public class Credentials {
 
-    private static String email = "br59241@gmail.com";
-    private static String password = "oDo94oZmbB";
+    protected static String email = "br59241@gmail.com";
+    protected static String password = "oDo94oZmbB";
 
     public static String getEmail() {
         return email;
